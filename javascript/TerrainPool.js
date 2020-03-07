f24007fbb2ab57d08d4f1fe11f0b5dcba2b55cf5
Marianna <janczak.mariannal@gmail.com>
2020-03-07 21:40:58 +0100

    generating terrein spans

---
+++ javascript/TerrainPool.js
@@ -0,0 +1,137 @@
+function TerrainPool() {
+    this.createWindows();
+    this.createDecorations();
+    this.createFrontEdges();
+    this.createBackEdges();
+    this.createSteps();
+}
+
+TerrainPool.prototype.borrowWindow = function() {
+    return this.windows.shift();
+};
+
+TerrainPool.prototype.returnWindow = function(sprite) {
+    this.windows.push(sprite);
+};
+
+TerrainPool.prototype.borrowDecoration = function() {
+    return this.decorations.shift();
+};
+
+TerrainPool.prototype.returnDecoration = function(sprite) {
+    this.decorations.push(sprite);
+};
+
+TerrainPool.prototype.borrowFrontEdge = function() {
+    return this.frontEdges.shift();
+};
+
+TerrainPool.prototype.returnFrontEdge = function(sprite) {
+    this.frontEdges.push(sprite);
+};
+
+TerrainPool.prototype.borrowBackEdge = function() {
+    return this.backEdges.shift();
+};
+
+TerrainPool.prototype.returnBackEdge = function(sprite) {
+    this.backEdges.push(sprite);
+};
+
+TerrainPool.prototype.borrowStep = function() {
+    return this.steps.shift();
+};
+
+TerrainPool.prototype.returnStep = function(sprite) {
+    this.steps.push(sprite);
+};
+
+TerrainPool.prototype.addWindowSprites = function(amount, frameId) {
+    for (var i = 0; i < amount; i++) {
+        var sprite = PIXI.Sprite.fromFrame(frameId);
+        this.windows.push(sprite);
+    }
+};
+
+TerrainPool.prototype.addDecorationSprites = function(amount, frameId) {
+    for (var i = 0; i < amount; i++) {
+        var sprite = new PIXI.Sprite(PIXI.Texture.fromFrame(frameId));
+        this.decorations.push(sprite);
+    }
+};
+
+TerrainPool.prototype.addFrontEdgeSprites = function(amount, frameId) {
+    for (var i = 0; i < amount; i++) {
+        var sprite = new PIXI.Sprite(PIXI.Texture.fromFrame(frameId));
+        this.frontEdges.push(sprite);
+    }
+};
+
+TerrainPool.prototype.addBackEdgeSprites = function(amount, frameId) {
+    for (var i = 0; i < amount; i++) {
+        var sprite = new PIXI.Sprite(PIXI.Texture.fromFrame(frameId));
+        sprite.anchor.x = 1;
+        sprite.scale.x = -1;
+        this.backEdges.push(sprite);
+    }
+};
+
+TerrainPool.prototype.addStepSprites = function(amount, frameId) {
+    for (var i = 0; i < amount; i++) {
+        var sprite = new PIXI.Sprite(PIXI.Texture.fromFrame(frameId));
+        sprite.anchor.y = 0.25;
+        this.steps.push(sprite);
+    }
+};
+
+TerrainPool.prototype.shuffle = function(arr) {
+    var len = arr.length;
+    var shuffles = len * 3;
+    for (var i = 0; i < shuffles; i++) {
+        var slice = arr.pop();
+        var pos = Math.floor(Math.random() * (len -1));
+        arr.splice(pos, 0, slice);
+    }
+};
+
+TerrainPool.prototype.createWindows = function() {
+    this.windows = [];
+
+    this.addWindowSprites(6, "window_01");
+    this.addWindowSprites(6, "window_02");
+
+    this.shuffle(this.windows);
+};
+
+TerrainPool.prototype.createDecorations = function() {
+    this.decorations = [];
+
+    this.addDecorationSprites(6, "decoration_01");
+    this.addDecorationSprites(6, "decoration_02");
+    this.addDecorationSprites(6, "decoration_03");
+
+    this.shuffle(this.decorations);
+};
+
+TerrainPool.prototype.createFrontEdges = function() {
+    this.frontEdges = [];
+
+    this.addFrontEdgeSprites(2, "edge_01");
+    this.addFrontEdgeSprites(2, "edge_02");
+
+    this.shuffle(this.frontEdges);
+};
+
+TerrainPool.prototype.createBackEdges = function() {
+    this.backEdges = [];
+
+    this.addBackEdgeSprites(2, "edge_01");
+    this.addBackEdgeSprites(2, "edge_02");
+
+    this.shuffle(this.backEdges);
+}
+
+TerrainPool.prototype.createSteps = function() {
+    this.steps = [];
+    this.addStepSprites(2, "step_01");
+};
--- javascript/main.js
+++ javascript/main.js
@@ -4,6 +4,7 @@ function Main() {
     this.evePath = 'eve.png';
     this.farBgPath = 'bg-far.png';
     this.midBgPath = 'bg-mid.png';
+    this.terrainConfPath = 'wall.json'
 
     this.defaultWidth = 512;
     this.defaultHeight = 384;
@@ -44,18 +45,21 @@ function Main() {
 
     this.gameLoop.bind(this);
 
-}
+};
 
 Main.prototype.loadAssets = function() {
     this.loader.baseUrl = '../assets/images';
     this.loader.add('eve', this.evePath)
         .add('bgFar', this.farBgPath)
         .add('bgMid', this.midBgPath)
+        .add('terrainConf', this.terrainConfPath)
         .on('progress', this.handleLoadProgress.bind(this))
         .on('load', this.handleLoadAsset.bind(this))
         .on('error', this.handleLoadError.bind(this))
         .load(this.handleLoadComplete.bind(this));
-}
+
+    this.loader.load();  
+};
 
 Main.prototype.handleLoadComplete = function() {
 
@@ -64,13 +68,18 @@ Main.prototype.handleLoadComplete = function() {
     // Eve
     this.eve = new Player(this.stage, this.loader.resources, this.startEveX, this.startEveY);
 
+
+    //terrain
+    // this.pool = new TerrainPool();
+    // this.slices = [];
+
     // Render stage
     this.renderer.render(this.stage);
 
     // Start gameLoop
     this.ticker.add(this.gameLoop.bind(this));
     this.ticker.start();
-}
+};
 
 Main.prototype.gameLoop = function() {
 
@@ -100,41 +109,98 @@ Main.prototype.gameLoop = function() {
             this.isJumping = false
         }
     }
-}
+};
 
 Main.prototype.setCanvasWidth = function() {
     if (window.innerWidth * 0.5 < this.defaultWidth) {
         this._x = window.innerWidth * 0.5;
     }
-}
+};
 
 Main.prototype.addEventListeners = function() {
     window.addEventListener('resize', this.resize.bind(this));
     window.addEventListener('keydown', this.keysDown.bind(this));
     window.addEventListener('keyup', this.keysUp.bind(this));
-}
+};
 
 Main.prototype.resize = function() {
     this.setCanvasWidth();
     this.renderer.resize(this._x, this._y);
-}
+};
 
 Main.prototype.keysDown = function(e) {
     this.activeKeys[e.keyCode] = true;
-}
+};
 
 Main.prototype.keysUp = function(e) {
     this.activeKeys[e.keyCode] = false;
-}
+};
 
 Main.prototype.handleLoadAsset = function(loader, resource) {
     console.log('asset loaded: ' + resource.name);
-}
+};
 
 Main.prototype.handleLoadProgress = function(loader, resource) {
     console.log(loader.progress + ' %loaded');
-}
+};
 
 Main.prototype.handleLoadError = function() {
     console.error('loading error');
-}
+};
+
+// Main.prototype.generateTestTerrainSpan = function() {
+
+//     this.terrainSlices = [];
+
+//     var lookupTable = [
+//         this.pool.borrowFrontEdge,  // 1st slice
+//         this.pool.borrowWindow,     // 2nd slice
+//         this.pool.borrowDecoration, // 3rd slice
+//         this.pool.borrowStep,       // 4th slice
+//         this.pool.borrowWindow,     // 5th slice
+//         this.pool.borrowBackEdge    // 6th slice
+//     ];
+
+//     var yPos = [
+//         128, // 1st slice
+//         128, // 2nd slice
+//         128, // 3rd slice
+//         192, // 4th slice
+//         192, // 5th slice
+//         192  // 6th slice
+//     ];
+
+//     for (var i =0; i < lookupTable.length; i++) {
+//         var func = lookupTable[i];
+
+//         var sprite = func.call(this.pool);
+
+//         sprite.x = 64 + (i * 64);
+//         sprite.y = yPos[i];
+
+//         this.terrainSlices.push(sprite);
+
+//         this.stage.addChild(sprite);
+//     };
+// };
+
+// Main.prototype.clearTestTerrainSpan = function() {
+//     var lookupTable = [
+//         this.pool.returnFrontEdge,  // 1st slice
+//         this.pool.returnWindow,     // 2nd slice
+//         this.pool.returnDecoration, // 3rd slice
+//         this.pool.returnStep,       // 4th slice
+//         this.pool.returnWindow,     // 5th slice
+//         this.pool.returnBackEdge    // 6th slice
+//     ];
+
+//     for (var i = 0; i < lookupTable.length; i++) {
+//         var func = lookupTable[i];
+//         var sprite = this.terrainSlices[i];
+
+//         this.stage.removeChild(sprite);
+//         func.call(this.pool, sprite);
+//     }
+
+//     this.terrainSlices = [];
+// };
